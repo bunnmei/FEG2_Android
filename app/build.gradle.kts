fda73ddc5b321fb01c8de156d84aa008b3994323
bunnmei <kombinat.web@gmail.com>
2025-09-03 20:43:50 +0900

    background locationのパーミッションの削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "space.webkombinat.feg2"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
